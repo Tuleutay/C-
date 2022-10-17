fda17d08f4b58f67a15e808cbb71504c03d460bf
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-10-17 20:45:13 +0600

    HomeWork001

---
+++ Дом_раб_001/Задача_2.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число А ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA < numberB)
+    {
+        Console.Write("max = ");
+        Console.WriteLine(numberB);
+    }
+if (numberA > numberB)
+    {
+        Console.Write("max = ");
+        Console.WriteLine(numberA);
+    }
+if (numberA == numberB)
+    {
+        Console.WriteLine("Числа ровны");
+    }
