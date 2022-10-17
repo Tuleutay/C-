cb928ac59f9864b9bee0d5969fd0d9e516f21c80
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-10-17 22:41:36 +0600

    задача 8

---
+++ Дом_раб_001/Задача_8/Задача_8.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i <= number; i += 2)
+{
+    Console.Write( i );
+    Console.Write(", ");
+}
