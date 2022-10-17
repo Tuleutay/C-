eafda652f6337d4ab7d0e78fe81b4664eace5d79
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-10-17 22:28:25 +0600

    задача 6

---
+++ Дом_раб_001/Задача_4/Задача_4.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberB > max)
+    {
+        max = numberB;
+    }
+if (numberC > max)
+    {
+        max = numberC;
+    }
+Console.Write("max = ");
+Console.WriteLine(max);
---
+++ Дом_раб_001/Задача_6/Задача_6.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int Q = number % 2;
+if (Q > 0)
+    {
+        Console.WriteLine("нет");
+    }
+if (Q == 0)
+    {
+       Console.WriteLine("да");
+    }
+
