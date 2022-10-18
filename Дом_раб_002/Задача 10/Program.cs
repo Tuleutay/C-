df5149c8fe105d674f5e5b3f11fcc04ec44074ff
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-10-19 00:01:07 +0600

    ДЗ_002 начало

---
+++ Дом_раб_002/Задача 10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int[] array = Convert.ToInt32(Console.ReadLine());
+int lengh = array.Lenght;
+
+if (lengh == 3) Console.WriteLine(array[1]);
+if (lengh > 3) Console.WriteLine("Ошибка");
+if (lengh < 3) Console.WriteLine("Ошибка");
+
+
+
