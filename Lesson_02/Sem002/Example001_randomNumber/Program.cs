2be388aceeee4581f15938ac059d9e24c2b612a3
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-10-19 00:09:58 +0600

    Sem2 Method randomNumber class Random

--- Lesson_02/Sem002/Example001_randomNumber/Program.cs
+++ Lesson_02/Sem002/Example001_randomNumber/Program.cs
@@ -1,3 +1,8 @@
-﻿Random random = new Random();
-int randomNumber = random.Next(10,100); //крайнее значение(99) не входит 
+﻿int randomNumber = GetRandomNumber(10, 100);
+int GetRandomNumber(int minValue, int maxValue)
+{
+    Random random = new Random();
+    int randomNumber = random.Next(minValue, maxValue);
+    return randomNumber;
+}
 Console.WriteLine(randomNumber);
