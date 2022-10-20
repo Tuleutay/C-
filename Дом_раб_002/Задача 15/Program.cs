5443862f0996eb2bd10a1d7c85b85f38dd468c93
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-10-21 01:02:07 +0600

    Дом работа 002 задача  15

---
+++ Дом_раб_002/Задача 15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("напишите цифру");
+int number = Convert.ToInt32(Console.ReadLine());
+bool DayOff(int number)
+{
+   int sun = number % 7;
+   int sat = number % 6;
+   return sun == 0 | sat == 0;
+}
+
+bool result = DayOff(number);
+if (result) Console.WriteLine("Да");
+else Console.WriteLine("нет");
