47c261ce4d0e6078b9e2adf7ccfccd43ffaf4dba
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-10-21 00:26:13 +0600

    Дом работа 002 задача  13

--- Дом_раб_002/Задача 13/Program.cs
+++ Дом_раб_002/Задача 13/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+ int[] array = new int[5];
+
+for (int i = 0; i < array.Length; i++)
+{
+    int lastValue = number % 10;
+    array[i] = lastValue;
+    number = number / 10;
+   }
+if (array[2]==0 & array[3]==0 & array[4]==0 & array[1]==0) Console.WriteLine("Третьей цифры нет");
+else if (array[2]==0 & array[3]==0 & array[4]==0) Console.WriteLine("Третьей цифры нет");
+else if (array[3]==0 & array[4]==0) Console.WriteLine(array[0]);
+else if (array[4]==0) Console.WriteLine(array[1]);
+else Console.WriteLine(array[2]);
