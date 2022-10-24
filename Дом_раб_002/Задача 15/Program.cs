1f72e584b529c9933b5a3b03409f87f24efbe74b
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-10-24 20:38:47 +0600

    Дом работа 002 задача  15-1

--- Дом_раб_002/Задача 15/Program.cs
+++ Дом_раб_002/Задача 15/Program.cs
@@ -2,9 +2,7 @@
 int number = Convert.ToInt32(Console.ReadLine());
 bool DayOff(int number)
 {
-   int sun = number % 7;
-   int sat = number % 6;
-   return sun == 0 | sat == 0;
+   return number == 6 | number == 7;
 }
 
 bool result = DayOff(number);
