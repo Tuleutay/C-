b2e9bdf53762dcdf4a176ab6dbd00535bf30d9d6
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-10-24 20:41:13 +0600

    sem03

---
+++ Lesson_03/sem_03/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool isParsedX = int.TryParse(Console.ReadLine(), out int x);
+int y = Convert.ToInt32(Console.ReadLine());
+
+int DetermineNumberOfQuarter(int x, int y)
+
+if 
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }
+    if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+    if (x < 0 && y < 0)
+    {
+        return 3;
+    }
+    if (x > 0 && y > 0)
+    {
+        return 4;
+    }
+    
+}
