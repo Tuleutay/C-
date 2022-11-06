88758b4d92118b4737505cfae77ecb4746cf16d3
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-11-06 19:16:09 +0600

    sem03_z21

---
+++ Lesson_03/sem03_zZ/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 21 - Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние м/у ними в 2D пространстве.
+
+double Distance(int x1, int y1, int x2, int y2)
+{
+    double dis = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    return dis;
+}
+Console.WriteLine("Введите координаты точ. A");
+bool isParsedXA = int.TryParse(Console.ReadLine(), out int xA);
+bool isParsedYA = int.TryParse(Console.ReadLine(), out int yA);
+Console.WriteLine("Введите координаты точ. B");
+bool isParsedYB = int.TryParse(Console.ReadLine(), out int yB);
+bool isParsedXB = int.TryParse(Console.ReadLine(), out int xB);
+if (!isParsedXA || !isParsedYA || !isParsedXB || !isParsedYB)
+{
+    Console.WriteLine("Ошибка!!! Не правильно введены координаты");
+    return;
+}
+
+double distanceS = Distance(xA, yA, xB, yB);
+Console.WriteLine($"Расстояние м/у точками {distanceS}");
