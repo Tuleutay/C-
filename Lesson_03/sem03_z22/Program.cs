8e7efcaba33c5c15b9757118edf73b52a9f81077
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-11-06 19:28:15 +0600

    sem03_z22

---
+++ Lesson_03/sem03_z22/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 22 - Напишите программу, которая 
+//принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+
+void TableOfSquares(int number)
+{
+    int[] array = new int[number + 1];
+    for (int i=1; i <= number; i++)
+    {
+        array[i] = i * i;
+        Console.Write($"{array[i]} ");
+    }
+}
+Console.WriteLine("Введите число");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int N);
+if (!isParsedN)
+{
+    Console.WriteLine("Ошибка! Это не число");
+    return;
+}
+TableOfSquares(N);
