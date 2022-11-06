099f270adc0f14829c76f31f57dfcd170e449b85
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-11-06 23:27:38 +0600

    ДЗ_задача 23

---
+++ Дом_раб_003/Задача 23/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 23 - напишите программу, которая принимает на вход число(N) 
+//и выдает таблицу кубов чисел от 1 до N
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] GetTableOfCube(int n)
+{
+    int [] array = new int [n];
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i] = (i+1)*(i+1)*(i+1);
+    }
+    return array;
+}
+int [] result = GetTableOfCube(n);
+PrintArray(result);
+void PrintArray(int [] array)
+{
+    for (int i=0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+}
+
