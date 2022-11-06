6c93c0e8d40038fd8c3485c85b88c09f45c959ab
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-11-06 19:57:45 +0600

    sem03_z22v2

---
+++ Lesson_03/sem03_z22v2array/Program.cs
@@ -0,0 +1,27 @@
+﻿// // Задача 22 - Напишите программу, которая 
+//принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// метод массива возведения в квадрат чисел от 1 до N
+int [] GetTableOfSquares(int n)
+{
+    int [] array = new int [n];
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i] = (i+1)*(i+1);
+    }
+    return array;
+}
+
+// метод вывода массива в консоль
+int [] result = GetTableOfSquares(n);
+PrintArray(result);
+void PrintArray(int [] array)
+{
+    for (int i=0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+}
