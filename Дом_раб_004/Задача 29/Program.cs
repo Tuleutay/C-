9683b1acd2bff2c8bbc67a219cd2ff80983038df
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-11-08 01:46:49 +0600

    Дом_Работа004-z29-

--- Дом_раб_004/Задача 29/Program.cs
+++ Дом_раб_004/Задача 29/Program.cs
@@ -1,20 +1,14 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
 //Задание должно быть выполнено в методах.
-Console.WriteLine("Введите восемь чисел");
-bool isParsedA = int.TryParse(Console.ReadLine(), out int a);
-bool isParsedB = int.TryParse(Console.ReadLine(), out int b);
-bool isParsedC = int.TryParse(Console.ReadLine(), out int c);
-bool isParsedD = int.TryParse(Console.ReadLine(), out int d);
-bool isParsedE = int.TryParse(Console.ReadLine(), out int e);
-bool isParsedF = int.TryParse(Console.ReadLine(), out int f);
-bool isParsedG = int.TryParse(Console.ReadLine(), out int g);
-bool isParsedH = int.TryParse(Console.ReadLine(), out int h);
-if(!isParsedA || !isParsedB || !isParsedC || !isParsedD || !isParsedE || !isParsedF || !isParsedG || !isParsedH)
+Console.WriteLine("Введите размер массива");
+bool isParsed = int.TryParse(Console.ReadLine(), out int lenght);
+
+if(!isParsed)
 {
     Console.WriteLine("Не правильно введены данные");
     return;
 }
-int[] arr = { a, b, c, d, e, f, g, h};
+int [] arr = 
 
 void PrintArray(int[] array)
 {
