ba97a7c95d04b131897e59068d85d47a521ca3c4
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-11-08 01:20:58 +0600

    Дом_Работа004-z25,27

---
+++ Дом_раб_004/Задача 25/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+//Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль.
+//
+//3, 5 -> 243 (3⁵)
+//
+//2, 4 -> 16
+
+Console.WriteLine("Введите числа A и B");
+bool isParsedA = int.TryParse(Console.ReadLine(), out int a);
+bool isParsedB = int.TryParse(Console.ReadLine(), out int b);
+if(!isParsedA || !isParsedB)
+{
+    Console.WriteLine("Не правильно введены числа");
+    return;
+}
+int Exponentiation(int a, int b)
+{
+    int s = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        s*= a;
+    }
+    return s;
+}
+Console.Write(Exponentiation(a, b));
---
+++ Дом_раб_004/Задача 27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+//Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль.
+Console.WriteLine("Введите число");
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+if(!isParsed)
+{
+    Console.WriteLine("Не правильно введены данные");
+    return;
+}
+if(number < 1)
+{
+    Console.WriteLine("0");
+    return;
+}
+Console.Write(GetSumNumbers(number));
+
+int GetSumNumbers(int number)
+{
+    int sumNum = 0;
+    while(number > 0)
+    {
+        sumNum = sumNum + number%10;
+        number /= 10;
+    }
+
+    return sumNum;
+}
