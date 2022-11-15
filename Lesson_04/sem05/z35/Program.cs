727bc3cb01d6ef9eb7b42ca91f8e31a606c1a171
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-11-16 00:00:53 +0600

    Zadacha 35

---
+++ Lesson_04/sem05/z35/Program.cs
@@ -0,0 +1,47 @@
+﻿//  Задайте одномерный массив из 123 случайных чисел.
+//Найдите количество элементов массива, значения которых лежат в
+//отрезке [10,99].
+//Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+//[5, 18, 123, 6, 2] -> 1
+//[1, 2, 3, 6, 2] -> 0
+//[10, 11, 12, 13, 14] -> 5
+
+int[] numbers = new int[123];
+FillArray(numbers);
+PrintArray(numbers);
+int count = GetCountNumberInRange(numbers);
+Console.WriteLine(count);
+
+
+
+int GetCountNumberInRange(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] <= 99 && array[i] >= 10)
+        {
+            count+=1;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine();
+    for (var i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 124);
+    }
+}
