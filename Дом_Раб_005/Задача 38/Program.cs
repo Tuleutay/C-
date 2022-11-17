3cd274fe4440c901d97448c1d6159c5ec06f7556
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-11-18 01:00:24 +0600

    HW_Zadacha38

---
+++ Дом_Раб_005/Задача 38/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+
+Console.WriteLine("Введите количество элементов массива");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.Write(SumMaxMinNubers(array));
+
+
+int SumMaxMinNubers(int[] array)
+{
+    int maxNum = array[0];
+    int minNum = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+      if(maxNum < array[i])
+      {
+        maxNum = array[i];
+      }
+      if(minNum > array[i])
+      {
+        minNum = array[i];
+      }
+    }
+    
+    int sum = maxNum + minNum;
+    return sum;
+}
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
