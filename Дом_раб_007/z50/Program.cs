cca302ec27b67cc1390858457abb19e3ed080f8f
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-11-24 10:28:46 +0600

    HWz50

--- Дом_раб_007/z50/Program.cs
+++ Дом_раб_007/z50/Program.cs
@@ -12,3 +12,60 @@
 1 , 7 -> такого числа в массиве нет
 1 , 1 -> 1 */
 
+Console.WriteLine("Создаем двумерный массив: введите m - ");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите n - ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+
+Console.WriteLine("Введите позиции элемента в двумерном массиве.");
+Console.WriteLine("Введите номер строки эелемента");
+bool isParsedX = int.TryParse(Console.ReadLine(), out int x);
+Console.WriteLine("Введите номер столбца эелемента");
+bool isParsedY = int.TryParse(Console.ReadLine(), out int y);
+
+if (!isParsedX || !isParsedY)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+int[,] array = CreateRandom2DArray(m,n);
+Print2DArray(array);
+if (x>m | y>n) Console.WriteLine("В данном масиве, нет такого элемента");
+else Console.Write($"{array[x-1,y-1]} ");
+
+
+
+int[,] CreateRandom2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = random.Next(1, 10);
+        }
+    }
+
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
