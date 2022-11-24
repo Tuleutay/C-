e457fe3472d503e73122e79736b205c5ee09fcbf
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-11-24 23:12:13 +0600

    HWz54

---
+++ Дом_раб_008/HWz54/Program.cs
@@ -0,0 +1,99 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите метод, 
+который упорядочит по убыванию элементы каждой строки двумерного массива. И продемонстрируйте его работу.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+
+Console.WriteLine("Введите количество строк");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите количество столбцов");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+
+int[,] array = CreateRandom2DArray(m, n);
+Print2DArray(array);
+
+Console.WriteLine();
+
+SortDescendingRow(array);
+Print2DArray(array);
+
+void SortDescendingRow(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = array.GetLength(1)-1; j >=0; j--)
+        {
+            int maxPosition = j;
+            for (int k = j - 1; k >=0; k--)
+            {
+                if (array[i,k] < array[i,maxPosition])
+                {
+                    maxPosition = k;
+                }
+            }
+            int temporary = array[i,j];
+            array[i,j] = array[i,maxPosition];
+            array[i,maxPosition] = temporary;
+        }
+    }
+}
+
+/*void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}*/
+
+
+int[,] CreateRandom2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
