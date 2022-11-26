e59351f03d65bca93d804225f01494ba665cbf10
Tuleutay <tuleutay.dairbekov@gmail.com>
2022-11-26 16:46:46 +0600

    HWz66

---
+++ Дом_раб_009/z66/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите метод, который найдёт сумму натуральных элементов в промежутке от M до N. Через рекурсию!
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+
+Console.WriteLine("Введите M");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите N");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+int result = GetSumNumbers(m,n);
+Console.Write(result);
+
+int GetSumNumbers(int m, int n)
+{
+    int sum = 0;
+     
+    if (m<=n)
+    {
+        
+        sum += m; //1 3
+        m++;//2 3
+        sum += GetSumNumbers(m,n);
+        
+    }
+    return sum;
+}
